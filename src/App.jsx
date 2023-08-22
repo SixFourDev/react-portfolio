19806a0de1108193d0df6fd6c0dcca9114ba8871
Shane <sb_94@yahoo.com>
2023-08-22 13:19:00 -0700

    set up App.jsx with imports and basic formatting

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Home />
+            <Portfolio />
+            <Contact />
+        </div>
+    );
+}
+
+export default App;
