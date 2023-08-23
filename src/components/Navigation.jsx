afa7b604c7bfeb9d0850ec460e937ce4fc05526b
Shane <sb_94@yahoo.com>
2023-08-23 11:09:32 -0700

    handled navigation useState for corresponding sections

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Navigation() {
+    const [ selected, setSelected ] = useState('about');
+
+    const handleNavClick = (section) => {
+        setSelected(section);
+        const element = document.getElementById(section);
+        element.scrollIntoView({ behavior: 'smooth' });
+
+    }
     return (
         <nav>
-            <ul>
-                <li><a href="#about">About me</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#resume">Resume</a></li>
-            </ul>
-        </nav>
+      <ul>
+        <li>
+          <a
+            href="#about"
+            onClick={() => handleNavClick('about')}
+            className={selected === 'about' ? 'selected' : ''}
+          >
+            About Me
+          </a>
+        </li>
+        <li>
+          <a
+            href="#portfolio"
+            onClick={() => handleNavClick('portfolio')}
+            className={selected === 'portfolio' ? 'selected' : ''}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contact"
+            onClick={() => handleNavClick('contact')}
+            className={selected === 'contact' ? 'selected' : ''}
+          >
+            Contact
+          </a>
+        </li>
+        <li>
+          <a
+            href="#resume"
+            onClick={() => handleNavClick('resume')}
+            className={selected === 'resume' ? 'selected' : ''}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
     );
 }
 
