fa024969312eb9f65e18cf8e92145683a188c957
Shane <sb_94@yahoo.com>
2023-08-23 10:34:25 -0700

    added about me section and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 // import Portfolio from './pages/Portfolio';
 // import Contact from './pages/Contact';
 
@@ -8,8 +8,8 @@ function App() {
     return (
         <div>
             <Header />
-            {/* <Home />
-            <Portfolio />
+            <Home />
+            {/* <Portfolio />
             <Contact /> */}
         </div>
     );
